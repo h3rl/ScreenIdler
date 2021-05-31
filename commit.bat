11e6926588cf8fd589dd9067bd0d3928d2b36874
Lazybaxy <halvard.erla@gmail.com>
2021-05-31 16:16:22 +0200

    remove unused

--- commit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if %1.==. (
-	echo No parameters have been provided.
-	echo Provide commit message ^(type string^)
-) else (
-	git add -A
-	git commit -a -m %1
-	git push
-)
--- hard.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-git fetch origin && git reset --hard origin/main && git clean -f -d
-
-
-setlocal enabledelayedexpansion
-set testl=%cmdcmdline:"=%
-set testr=!testl:%~nx0=!
-if not "%testl%" == "%testr%" pause
