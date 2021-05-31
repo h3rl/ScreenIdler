2dd5403e89404463dd53601f262adf7d04b710c8
Lazybaxy <halvard.erla@gmail.com>
2021-05-31 16:10:10 +0200

    major update

--- main.cpp
+++ main.cpp
@@ -4,76 +4,101 @@
 #include <thread>
 #include <chrono>
 
-/*
-int WINAPI WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nShowCmd
-)
-{
-	WNDCLASS wc = { };
-
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.lpszClassName = CLASS_NAME;
-
-	RegisterClass(&wc);
-
-	HWND hwnd = CreateWindowExW(
-		NULL,
-		L"screenIdleClass",
-		L"Serpentine's Auto Farmer V 1.0",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 300, 200,
-		NULL, NULL, hInstance, NULL);
-
-	if (hwnd == NULL)
-	{
-		MessageBox(NULL, "Failed To Create Window!", "Error!",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
-
-	ShowWindow(hwnd, SW_MAXIMIZE); //<<<<<<<<<<<<<<<<<<<<<<<<<<<
-	//UpdateWindow(hwnd);
-	while (true)
-	{
-		if (GetAsyncKeyState(VK_ESCAPE))
-		{
-			return 1;
-		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	}
-}
-*/
-
+#include "debug.h"
 
 // compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
 
-#include <windows.h>
 #include <stdlib.h>
 #include <string.h>
-#include <tchar.h>
 
 // Global variables
 
 // The main window class name.
-static TCHAR szWindowClass[] = _T("win32app");
+static TCHAR szWindowClass[] = _T("ScreenIdlerClass");
 
 // The string that appears in the application's title bar.
-static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
-
-HINSTANCE hInst;
+static TCHAR szTitle[] = _T("ScreenIdler");
 
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void doExit(int eCode = 0);
+
+bool bexit = false;
+
+int monitors = 1;
+
+HINSTANCE hInst = NULL;
+HHOOK kbHook = NULL;
+
+BOOL CALLBACK kbProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode < 0) {
+        return CallNextHookEx(kbHook, nCode, wParam, lParam);
+    }
+
+    LPKBDLLHOOKSTRUCT keyStruct = (LPKBDLLHOOKSTRUCT)lParam;
+
+    if ((keyStruct->flags >> LLKHF_INJECTED) & 1) {
+        // ignore injected events
+    }
+	else {
+        bexit = true;
+	}
+	return CallNextHookEx(kbHook, nCode, wParam, lParam);
+
+}
+
+BOOL CALLBACK MyInfoEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+    MONITORINFOEX info;
+    info.cbSize = sizeof(info);
+    if (GetMonitorInfo(hMonitor, &info))
+    {
+
+    }
+
+    HWND hWnd = CreateWindowA(
+        szWindowClass,
+        szTitle,
+        WS_MAXIMIZE | WS_POPUP | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL,
+        NULL,
+        NULL,
+        hInst,
+        NULL
+    );
+
+    if (!hWnd)
+    {
+        MessageBoxA(NULL,
+            _T("Call to CreateWindow failed!"),
+            szTitle,
+            NULL);
+
+        return 1;
+    }
+
+    int x = info.rcMonitor.left;
+    int y = info.rcMonitor.top;
+    int cx = std::abs(info.rcMonitor.left - info.rcMonitor.right);
+    int cy = std::abs(info.rcMonitor.top - info.rcMonitor.bottom);
+    SetWindowPos(hWnd, NULL, x, y, cx, cy, SWP_NOREDRAW | SWP_SHOWWINDOW);
+    _D("Monitor " << monitors << ": " << cx << "x" << cy << 
+        " (" << x << "," << y << ")");
+    monitors += 1;
+    return TRUE;
+}
 
 int WINAPI WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
+    hInst = hInstance;
+    createDbgConsole();
+
     WNDCLASSEX wcex;
 
     wcex.cbSize = sizeof(WNDCLASSEX);
@@ -83,7 +108,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
     wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hCursor = NULL;
     wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
     wcex.lpszMenuName = NULL;
     wcex.lpszClassName = szWindowClass;
@@ -91,96 +116,58 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     if (!RegisterClassEx(&wcex))
     {
-        MessageBox(NULL,
+        MessageBoxA(NULL,
             _T("Call to RegisterClassEx failed!"),
-            _T("Win32 Guided Tour"),
+            szTitle,
             NULL);
 
         return 1;
     }
 
-    hInst = hInstance; // Store instance handle in our global variable
+    kbHook = SetWindowsHookExA(WH_KEYBOARD_LL, (HOOKPROC)kbProc, NULL, NULL);
 
-    // The parameters to CreateWindow explained:
-    // szWindowClass: the name of the application
-    // szTitle: the text that appears in the title bar
-    // WS_OVERLAPPEDWINDOW: the type of window to create
-    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-    // 500, 100: initial size (width, length)
-    // NULL: the parent of this window
-    // NULL: this application does not have a menu bar
-    // hInstance: the first parameter from WinMain
-    // NULL: not used in this application
-    HWND hWnd = CreateWindow(
-        szWindowClass,
-        szTitle,
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, NULL,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
+    EnumDisplayMonitors(NULL, NULL, MyInfoEnumProc, 0);
 
-    if (!hWnd)
-    {
-        MessageBox(NULL,
-            _T("Call to CreateWindow failed!"),
-            _T("Win32 Guided Tour"),
-            NULL);
-
-        return 1;
+    while (GetAsyncKeyState(VK_LBUTTON)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
-    // The parameters to ShowWindow explained:
-    // hWnd: the value returned from CreateWindow
-    // nCmdShow: the fourth parameter from WinMain
-    ShowWindow(hWnd, SW_MAXIMIZE); //<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    UpdateWindow(hWnd);
-
-    // Main message loop:
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
-    }
 
-    return (int)msg.wParam;
+        if (bexit || GetAsyncKeyState(VK_LBUTTON) || GetAsyncKeyState(VK_RBUTTON) || GetAsyncKeyState(VK_MBUTTON))
+            break;
+    }
+    doExit();
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
+void doExit(int eCode)
+{
+    if (kbHook) {
+        UnhookWindowsHookEx(kbHook);
+    }
+    UnregisterClassA(szWindowClass, hInst);
+    ExitProcess(eCode);
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
-    TCHAR greeting[] = _T("Hello, World!");
 
     switch (message)
     {
-    /*
-    case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
-
-        // Here your application is laid out.
-        // For this introduction, we just print out "Hello, World!"
-        // in the top left corner.
-        // End application-specific layout section.
-
-        EndPaint(hWnd, &ps);
+    case WM_SETCURSOR:
+    {
+        if (LOWORD(lParam) == HTCLIENT)
+        {
+            SetCursor(NULL);
+        }
         break;
-    */
+    }
     case WM_DESTROY:
-        PostQuitMessage(0);
+        doExit(1);
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
